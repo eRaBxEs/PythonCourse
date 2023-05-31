ca3c30494c04fcd2a1c224db5d8d9ca02470be3d
Ehioje Henry Erabor <ehiojee@yahoo.com>
2023-05-31 18:14:45 +0100

    another interactive program

---
+++ tut4/interactive2.py
@@ -0,0 +1,14 @@
+greetings = "Hello "
+
+name = input("Enter your name:")
+
+age = input("Enter your age as a number:")
+
+questions = "Are you ready for python?"
+
+affirm = " Your age of "
+
+affirm2 = " does not matter"
+
+
+print(greetings + name +". "+questions + affirm + age + affirm2)
