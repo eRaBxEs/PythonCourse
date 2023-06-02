9c52d3ea140585464ac0f273f5eaccb0fdd7c239
Ehioje Henry Erabor <ehiojee@yahoo.com>
2023-06-02 22:28:59 +0100

    convert from Fah to Cel program

---
+++ tut10/converting_temperatures.py
@@ -0,0 +1,6 @@
+temperature_f = input("Enter the temperature in Fahrenheit: ")
+
+temperature_c = (float(temperature_f) - 32) * 0.5556
+
+
+print(temperature_f + " F is equal to " + str(temperature_c) + " C")
