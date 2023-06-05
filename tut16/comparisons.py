e12ebd4fe961658b85f962e0fd0fcb18691ced1e
Ehioje Henry Erabor <ehiojee@yahoo.com>
2023-06-05 18:59:07 +0100

    using comparison operators

---
+++ tut16/comparisons.py
@@ -0,0 +1,16 @@
+# ==, <, >, !=, <=, >= 
+
+print("cat" == "dog")
+print(5 > 6)
+print(5 < 6)
+
+print("cat" > "dog")
+print("cat" > "Dog")
+
+print(ord("c"))
+print(ord("D"))
+
+print("cat" != "dog")
+
+print(5 > 6)
+print(5 <= 5)
