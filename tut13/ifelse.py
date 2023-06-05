deaa048dfe4be2adf3ff9e9b358afe9313ade886
Ehioje Henry Erabor <ehiojee@yahoo.com>
2023-06-05 18:57:41 +0100

    simple use of if else construct

---
+++ tut13/ifelse.py
@@ -0,0 +1,10 @@
+name = input("Enter your name:")
+
+if name == "Ehioje":
+    print("Your name is Ehioje!")
+    print("Hello")
+else:
+    print("Your name is not Ehioje")
+    
+
+print("Program finished")
