8378149c83d86d31cec6abe426e06b82a3131344
Ehioje Henry Erabor <ehiojee@yahoo.com>
2023-06-05 18:58:28 +0100

    using if elif construct in python

---
+++ tut15/ifelif.py
@@ -0,0 +1,16 @@
+WORKER1 = "Ehioje"
+WORKER2 = "Rachel"
+PROFESSOR = "Falcon"
+
+name = input("Enter your name:")
+
+if name == WORKER1:
+    print("Hello Ehioje!")
+elif name == WORKER2:
+    print("Ciao Rachel")
+elif name == PROFESSOR:
+    print("Greetings Professor Falcon")
+else:
+    print("Name not recognised!")
+    
+
