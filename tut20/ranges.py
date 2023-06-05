29d05e16ff1ea822618c589a5723390c8ef865f8
Ehioje Henry Erabor <ehiojee@yahoo.com>
2023-06-05 19:01:27 +0100

    show varying use of counter in for loop range

---
+++ tut20/ranges.py
@@ -0,0 +1,25 @@
+# i here represents the counter
+for i in range(4):
+    print(i)
+
+print()
+
+for i in range(1, 4):
+    print(i)
+
+print()
+
+for i in range(4, 11, 2):
+    print(i)
+
+print()
+
+# not interested in using the value of i the counter
+for _ in range(4, 11, 2):
+    print(_)
+
+print()
+
+# not interested in using the value of i the counter
+for _ in range(3):
+    print("Hello")
