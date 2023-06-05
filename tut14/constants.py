ba462c922aa661954e3e6247df9eb4e1a58575e7
Ehioje Henry Erabor <ehiojee@yahoo.com>
2023-06-05 18:58:06 +0100

    using constants in python

---
+++ tut14/constants.py
@@ -0,0 +1,12 @@
+NAME = "Ehioje"   # This signals to all that this is a constant
+
+name = input("Enter your name:")
+
+if name == NAME:
+    print("Your name is Ehioje!")
+    print("Hello")
+else:
+    print("Your name is not Ehioje")
+    
+
+print("Program finished")
