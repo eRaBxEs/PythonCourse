76ea3712ea06e95dc43f2b6d02d66ee013a02882
Ehioje Henry Erabor <ehiojee@yahoo.com>
2023-06-06 23:08:53 +0100

    boolean related question

---
+++ tut26/boolean-operators-exercise.py
@@ -0,0 +1,21 @@
+"""
+
+Write a program that asks user 3 questions:
+
+Ask the user:
+1. Are you a student?
+2. Do you have pets?
+3. Do you smoke?
+
+The program automatically decides whether a property that you've applied to rent is available to you.
+
+It should print an appropriate response, like "Property available" or "Property unavailable"
+
+The program applies these criteria:
+
+If you 're a student, you can only rent the property if you don't have a pet and if you don't smoke.
+
+If you 're not a student, you can rent the property if you smoke or have pets, but not if you
+smoke and also have pets
+
+"""
